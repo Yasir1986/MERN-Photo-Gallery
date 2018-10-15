b8cdd4ed6c2e87fb137d46dbbb8e6397ebd2c642
Yasir Akbar <yasir.akbar86@hotmail.com>
2018-10-15 14:21:57 +0300

    Update NewStudent.js

--- client/src/components/NewStudent.js
+++ client/src/components/NewStudent.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import './style.css';
 
-
 class NewStudent extends Component {
   render() {
     return (
       <div>
 
         <h2>Add New Students</h2>
-
          <form
          ref="uploadForm"
          id="uploadForm"
